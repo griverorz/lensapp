610ef71f0c7ce35a746ebe1f422f9b258dbf0486
Gonzalo Rivero <griverorz@gmail.com>
2018-04-03 09:55:54 -0400

    Async retrieval with redis

--- celeryconfig.py
+++ celeryconfig.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 broker_url = 'redis://redis:6379/0'
 result_backend = 'redis://redis:6379/0'
+
 result_persistent = True
 task_result_expires = None
 send_events = True
--- lensapp/config.py
+++ lensapp/config.py
@@ -3,20 +3,6 @@ from __future__ import absolute_import
 broker_url = 'redis://redis:6379/0'
 result_backend = 'redis://redis:6379/0'
 
-imports = ("lensapp.tasks",)
-
-result_persistent = True
-task_result_expires = None
-send_events = True
-enable_utc = True
-
-task_serializer = 'json'
-result_serializer = 'json'
-
-accept_content = ['json']
-timezone = 'America/New_York'
-redis_max_connections = 10
-
 
 class Config(object):
     DEBUG = False

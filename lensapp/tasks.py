6d473e3cb13f7e55224e378605b686ea1cd52cf3
Gonzalo Rivero <griverorz@gmail.com>
2018-04-18 20:07:32 -0400

    Retry tasks

--- lensapp/tasks.py
+++ lensapp/tasks.py
@@ -1,10 +1,15 @@
 from celery.task import task
 import time
 from matplotlib.image import imread
+import random
 
 
-@task
-def process_image(x):
-    img = imread(x)
-    time.sleep(25)
-    return(img.shape)
+@task(bind=True, max_retries=5)
+def process_image(self, x):
+    try:
+        img = imread(x)
+        time.sleep(25)
+        return(img.shape)
+    except Exception as e:
+        jittered = int(random.uniform(2, 4) ** self.request.retries)
+        self.retry(exc=e, countdown=jittered)
